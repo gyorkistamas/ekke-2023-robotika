0ae750c6e7b0df0ec346dddd94b5ae6795dfa906
Banyik Nándor <83366829+Banyik@users.noreply.github.com>
2023-05-20 12:28:45 +0200

    Added MotorHandler and (in test) SensorHandler

--- radar/radar.ino
+++ radar/radar.ino
@@ -1,8 +1,70 @@
-void setup() {
-  // put your setup code here, to run once:
+//#include <Stepper.h>
 
+#define trigger 9
+#define echo 10
+
+long duration;
+int distance;
+
+int stepsPerRevolution = 64;
+int fullRotation = 2038
+string state = "clockwise";
+Stepper myStepper(fullRotation, 6, 8, 7, 9);
+
+
+void setup() {
+
+  pinMode(trigger, OUTPUT);
+  pinMode(echo, INPUT);
+  myStepper.setSpeed(11);
+  Serial.begin(9600);
+}
+
+void loop() 
+{
+	MotorHandler();
+}
+
+void SensorHandler()
+{
+  digitalWrite(trigger, LOW);
+  delayMicroseconds(2);
+  digitalWrite(trigger, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trigger, LOW);
+  duration = pulseIn(echo, HIGH);
+  distance = duration * 0.034 / 2;
+  Serial.print("Distance: ");
+  Serial.println(distance);
+}
+
+float microsecondsToCentimeters(float microseconds) {
+   return microseconds / 29 / 2;
 }
 
 void MotorHandler(){
-  
+
+  switch(state){
+    case "clockwise":
+      stepsPerRevolution = -stepsPerRevolution;
+      limit = 0;
+      state = "counterClockwise"
+    break;
+    case "counterClockwise":
+      stepsPerRevolution = -stepsPerRevolution;
+      limit = fullRotation;
+      state = "clockwise";
+    break;
+  }
+
+  while(currStep < limit){
+    SetSpeed();
+    myStepper.step(stepsPerRevolution);
+    SensorHandler();
+  }
 }
+
+void SetSpeed(){
+  //potmeter
+}
+
